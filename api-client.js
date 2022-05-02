3c55341bec8138da9856391d05469a17a422852d
Merijn Vonk <merijnvonk@gmail.com>
2022-05-02 19:16:51 +0200

    almost done

--- api-client.js
+++ api-client.js
@@ -37,15 +37,58 @@ const getTasks = async () => {
     }
 }
 
-const deleteTask = async () => {
+const deleteTask = async listId => {
     const array = await getTasks();
     const settings = {
         method: 'DELETE'
     };
     try {
-        const response = await fetch(`http://localhost:3000/${smth}`, settings);
+        const response = await fetch(`http://localhost:3000/${listId}`, settings);
         const data = await response.json();
-        console.log(data);
+        return (data);
+        
+    } catch (error) {
+        console.log(error);        
+    }
+}
+
+const putTaskDone = async listId => {
+    const putTask = {done: true};
+    const settings = {
+        method: 'PUT',
+        body: JSON.stringify(putTask),
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+        }
+    };
+    try {
+        const response = await fetch(`http://localhost:3000/${listId}`, settings);
+        const data = await response.json();
+        return data;
+        
+    } catch (error) {
+        console.log(error);        
+    }
+}
+
+let update = ""
+
+const putTaskDescription = async listId => {
+    update = prompt("Change your task:")
+    const putTask = {description: update};
+    const settings = {
+        method: 'PUT',
+        body: JSON.stringify(putTask),
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+        }
+    };
+    try {
+        const response = await fetch(`http://localhost:3000/${listId}`, settings);
+        const data = await response.json();
+        return data;
         
     } catch (error) {
         console.log(error);        
--- script.js
+++ script.js
@@ -7,12 +7,12 @@ const toDoList = document.getElementById("todo-list");
 const createToDoList = async () => {
     const tasks = await getTasks();
     tasks.forEach(task => {
+        const taskLi = document.createElement("li");
+        const taskText = document.createTextNode(task.description);
+        
         const checkbox = document.createElement("input");
         checkbox.type="checkbox";
         checkbox.classList.add("checkbox");
-
-        const taskLi = document.createElement("li");
-        const taskText = document.createTextNode(task.description);
 
         const removeButtonIcon = document.createElement("i");
         removeButtonIcon.classList.add("fa-solid");
@@ -22,6 +22,19 @@ const createToDoList = async () => {
         taskLi.appendChild(checkbox);
         taskLi.appendChild(taskText);
         taskLi.appendChild(removeButtonIcon);
+
+        removeButtonIcon.addEventListener("click", () => {
+            deleteTask(task._id);
+        });
+
+        checkbox.addEventListener("click", () => {
+            putTaskDone(task._id);
+        });
+
+        taskText.addEventListener("click", event => {
+            console.log(event);
+            putTaskDescription(task._id);
+        });
     });
 }
 
@@ -34,12 +47,12 @@ const addTaskToList = async() => {
     const tasks = await getTasks();
     const taskToAd = tasks[tasks.length - 1];
 
+    const taskLi = document.createElement("li");
+    const taskText = document.createTextNode(taskToAd.description);
+
     const checkbox = document.createElement("input");
     checkbox.type="checkbox";
     checkbox.classList.add("checkbox");
-
-    const taskLi = document.createElement("li");
-    const taskText = document.createTextNode(taskToAd.description);
 
     const removeButtonIcon = document.createElement("i");
     removeButtonIcon.classList.add("fa-solid");
@@ -50,31 +63,50 @@ const addTaskToList = async() => {
     taskLi.appendChild(taskText);
     taskLi.appendChild(removeButtonIcon);
 
+    removeButtonIcon.addEventListener("click", () => {deleteTask(taskToAd._id)});
+    checkbox.addEventListener("click", () => {putTask(taskToAd._id)});
+
     document.getElementById("todo-item").value='';
 }
-
-// remove task from the list
-
-const removeTaskFromList = async event => {
-    const tasks = await getTasks();
-           
-}
-
 
 // event listener to add a task
 
 const addTaskButton = document.getElementById("add-task");
 addTaskButton.addEventListener("click", addTaskToList);
 
-// event listener to delete a task
+// remove task from the list
 
-const addEventListenerRemoveTask = () => {
-    const trashIcons = document.getElementsByClassName('fa-solid');
-    Array.from(trashIcons).forEach(icon => {
-        icon.addEventListener("toggle", removeTaskFromList)
+const removeTaskFromList = () => {
+    const removeButtonIcons = Array.from(document.getElementsByClassName("fa-solid"));
+    removeButtonIcons.forEach(button => {
+        button.addEventListener("click", event => {
+            event.path[1].remove();
+        });
     });
-};
+}
 
-setTimeout(addEventListenerRemoveTask, 1000);
+setInterval(removeTaskFromList, 100);
 
-// event listener to check a task
+// check task on the list
+
+const checkTaskFromList = () => {
+    const checkboxes = Array.from(document.getElementsByClassName("checkbox"));
+    checkboxes.forEach(box => {
+        box.addEventListener("click", event => {
+            event.path[1].classList.add("mark-done");
+        });
+    });
+}
+
+setInterval(checkTaskFromList, 100);
+
+const updateTaskInList = () => {
+    const text = Array.from(document.getElementsByTagName("li"));
+    text.forEach(li => {
+        li.addEventListener("click", event => {
+            console.log(event.target);
+        });
+    });
+}
+
+setTimeout(updateTaskInList, 1000);
